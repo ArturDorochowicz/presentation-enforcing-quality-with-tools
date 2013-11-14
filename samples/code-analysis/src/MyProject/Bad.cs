9147e11d7423d3b304f71fae9ec069de6162d35d
Artur Dorochowicz <artur@dorochowicz.com>
2013-11-14 20:02:38 +0100

    Suppress CA2000 with justification.

--- samples/code-analysis/src/MyProject/Bad.cs
+++ samples/code-analysis/src/MyProject/Bad.cs
@@ -13,6 +13,7 @@ namespace MyProject
         {
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "This is only to demonstrate how to suppress a violation. In reality this should be corrected.")]
         public static void Method()
         {
             using (var smtpClient = new SmtpClient { Host = "smtp.gmail.com" })
