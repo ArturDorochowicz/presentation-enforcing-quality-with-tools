e9f477efc6cf73395c0f9aea6076870719f16f2c
Artur Dorochowicz <artur@dorochowicz.com>
2013-11-14 20:45:43 +0100

    StyleCop violation and suppression.
    Run StyleCop with:
    msbuild.exe /p:StyleCopEnabled=true

---
+++ samples/style-cop/src/MyProject/Bad.cs
@@ -0,0 +1,14 @@
+﻿namespace MyProject
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:ElementsMustBeOrderedByAccess", Justification = "This is to demonstrate usage of suppression in StyleCop.")]
+    public class Bad
+    {
+        static void PrivateBeforePublic()
+        {
+        }
+
+        public static void Public()
+        {
+        }
+    }
+}
