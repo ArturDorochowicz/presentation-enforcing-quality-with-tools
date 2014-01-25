5f56b2ada1d6c861865b673d22fd6c0d09f70417
Artur Dorochowicz <artur@dorochowicz.com>
2014-01-25 23:18:33 +0100

    Normalize the path in JSHint reporter.
    This gets us consistent, Windows directory separators.
    Mixed separators seem to confuse VS sometimes which fails to recognize
    the file when clicking on error in the compilation output.

--- samples/jshint/src/jshint-msbuild-reporter.js
+++ samples/jshint/src/jshint-msbuild-reporter.js
@@ -3,12 +3,14 @@
 
 'use strict';
 
+var path = require('path');
+
 module.exports = {
    reporter: function (results) {
 
       results.forEach(function (result) {
          var error = result.error;
-         var origin = result.file + '(' + error.line + ',' + error.character + ')';
+         var origin = path.normalize(result.file) + '(' + error.line + ',' + error.character + ')';
          var category = error.id === '(error)' ? 'error' : error.id;
          var code = error.code;
          var text = error.reason;
@@ -17,3 +19,4 @@ module.exports = {
       });
    }
 };
+
